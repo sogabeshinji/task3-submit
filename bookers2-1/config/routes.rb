3df77ebf7f9c68369aec18b9b81e21a40ff50ab1
shinjisogabe <124783728+sogabeshinji@users.noreply.github.com>
2023-04-17 19:38:47 +0000

    新規ファイル作成のため

--- bookers2-1/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'homes#top'
-  get "/homes/about" => "homes#about", as: "about"
-  resources :books, only: [:create, :index, :show, :edit, :destroy] do
-  end
-  resources :users, only: [:index, :show, :edit, :update] do
-  end
-end
