9fe7b7ba61d3ee22de6020b04433c1b6deee42c3
shinjisogabe <124783728+sogabeshinji@users.noreply.github.com>
2023-04-17 19:34:56 +0000

    [finish]task3

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+  
+  def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
+  def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+  end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body).merge(user_id:current_user.id)
+  end
+  
+end
+
